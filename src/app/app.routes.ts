88774c4df7234d26ef5807cbd9fd2fcbfc990be2
Kimberlyandrade01 <kimberly.andradecervantes@cesunbc.edu.mx>
2025-10-28 09:14:25 -0700

    Original

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list-pokemons',
+    loadComponent: () => import('./pages/list-pokemons/list-pokemons.page').then( m => m.ListPokemonsPage)
+  },
+  {
+    path: 'pokemon-detail',
+    loadComponent: () => import('./pages/pokemon-detail/pokemon-detail.page').then( m => m.PokemonDetailPage)
+  },
+];
